58939fdcbd5029a81452d4121922a7f36b415764
mr mike <mickyebira@gmail.com>
2025-05-08 14:42:21 +0100

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import './header.css'
 import Nav from '../Nav/Nav'
+import back from '../../img/bgd-2.jpg'
 import { Link } from 'react-router-dom'
 
 function Header({content,Header,btn}) {
   return (
+    
     <div className='header'>
-      <Nav/>
+    <div className='bcimg'>
+      <img src={back} alt='demejus academy' width="100%"/>
+    </div>
+    <div className='headerc'>
+    <Nav/>
       <div className='content'>
       <h1>{Header}</h1>
       <p>{content} </p>
     {btn?(<Link to="/contact" className='hbutton'> <button className='button '>{btn}</button>
       </Link>):""} </div>
+    </div>
+     
       
     </div>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ import Footer from '../../components/footer/Footer'
 
 function Home() {
   return (<div>
-    <Header Header="DE_MEJUS ACADEMY" content="At DE-MEJUS ACADEMY, we provide a nurturing and academically rigorous environment for students in Abuja, Nigeria. Our international curriculum, experienced faculty, and strong STEM programs set us apart among the best private schools in Abuja." btn="contact us"/>
+    <Header Header="DE-MEJUS ACADEMY" content="At DE-MEJUS ACADEMY, we provide a nurturing and academically rigorous environment for students in Abuja, Nigeria. Our international curriculum, experienced faculty, and strong STEM programs set us apart among the best private schools in Abuja." btn="contact us"/>
     <Aboutsec/>
     <Clubs/>
     <CON/>
